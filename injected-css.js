af1f41fe80b99a7cd4d25124bf9eff8d9f20aae6
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2024-04-18 19:07:44 -0400

    Added javascript file changing text color, font and font size

---
+++ injected-css.js
@@ -0,0 +1,11 @@
+const style = document.createElement('style');
+style.innerHTML = `
+* {
+    color: cornflowerblue !important;
+    font-family: "Georgia" !important;
+}
+html {
+    font-size: 150%;
+}
+`;
+document.head.appendChild(style);
