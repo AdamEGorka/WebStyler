799b7ac5c71ef890b7912fcc017a8284d1529f21
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2024-05-10 00:19:58 -0400

    Add size changing
    Scales text size by recursing over the DOM body and resizing each element in it
    Size changes when pressing apply button
    Reset button resets text size
    Renamed apply() method to applyCSS()

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-// https://stackoverflow.com/questions/77495555/how-do-i-execute-a-script-on-the-current-tab-using-chrome-scripting-api:
-
 // getCurrentTabId() retrieves the ID of the currently active tab
 // in the last focused window,
 // which is necessary for sending messages to the correct tab.
@@ -12,11 +10,13 @@ async function getCurrentTabId() {
     }
 }
 
-// Gets size from size slider
+// Gets size from size slider, ranging from 1 to 100
 function getSize() {
-    var sizeSlider = document.getElementById("size-slider")
-    return sizeSlider.value;
+    var sizeSlider = document.getElementById("size-slider");
+    // Prevents returned size from being 0
+    return Math.max(sizeSlider.value, 1);
 }
+var currSize = 50;
 
 // Gets color from color wheel
 function getColor() {
@@ -35,9 +35,9 @@ function getCSS() {
 }
 var currCss = '';
 
-// Applies new CSS according to the specified size, spacing, color, etc.
-async function apply() {
-    let currTabId = await getCurrentTabId();
+// Applies new CSS according to the specified color
+async function applyCSS() {
+    var currTabId = await getCurrentTabId();
     // Removes old CSS before injecting new one to not flood website with old CSS
     removeCSS(currTabId);
     
@@ -55,11 +55,16 @@ async function apply() {
 
 // Resets the website's CSS to what it was initially
 async function reset() {
-    let currTabId = await getCurrentTabId();
-    removeCSS(currTabId);
+    getCurrentTabId().then((currTabId) => {
+        removeCSS(currTabId);
+    });
+    changeSize(currSize, 50).then(() => {
+        currSize = 50;
+        console.log("Size reset");
+    });
 }
 
-// Helper function to remove CSS. Separated from reset() to avoid recomputing currTabId in apply()
+// Helper function to remove CSS. Separated from reset() to avoid recomputing currTabId in applyCSS()
 async function removeCSS(currTabId) {
     try {
         chrome.scripting.removeCSS({
@@ -68,15 +73,70 @@ async function removeCSS(currTabId) {
         });
         console.log("CSS reset");
     } catch (err) {
-        console.error("Failed to reset CSS: ", err)
+        console.error("Failed to reset CSS: ", err);
     }
+}
+
+// Applies new size according to the specified size
+async function applySize() {
+    await changeSize(currSize, getSize());
+    // Updates currSize. Important that it's after the call to changeSize, executeScript in particular
+    currSize = getSize();
+}
+
+// Helper function to change size. Separated from applySize() to allow giving inputs in reset()
+async function changeSize(oldSize, newSize) {
+    chrome.tabs.query({
+        active: true, currentWindow: true
+    }).then(function (tabs) {
+        var activeTab = tabs[0];
+        var activeTabId = activeTab.id;
+
+        chrome.scripting.executeScript({
+            target: { tabId: activeTabId },
+            func: resizeBody,
+            args: [oldSize, newSize]
+        });
+        console.log("Size changed");
+    }).catch(function (error) {
+        console.log("There was an error injecting the script: " + error.message);
+    });
+}
+
+// Resizes elements in body
+function resizeBody(oldSize, newSize) {
+    /**
+     * Recursively resizes the given element and its children.
+     * @param {HTMLElement} elt - The element to resize.
+     */
+    function resizeChildren(elt) {
+        // Recurse over elt.children
+        for (var child of elt.children) {
+            // Notably avoids changing font size of parent affecting font size of children by changing children first
+            resizeChildren(child);
+        }
+        // Restyle current elt
+        var style = window.getComputedStyle(elt, null);
+        var fontSize = parseFloat(style.getPropertyValue('font-size'));
+        // Unscales by oldSize/50 and scales by newSize/50 at the same time
+        elt.style.fontSize = (fontSize / (oldSize/50) * (newSize/50)) + 'px';
+    }
+
+    body = document.querySelector("body");
+    resizeChildren(body);
 }
 
 // When DOM is loaded, adds event listeners to buttons
 document.addEventListener('DOMContentLoaded', function () {
-    var applyBtn = document.getElementById("apply-btn")
-    applyBtn.addEventListener('click', apply);
+    var applyBtn = document.getElementById("apply-btn");
+    applyBtn.addEventListener('click', applyCSS);
+    applyBtn.addEventListener('click', applySize);
 
-    var resetBtn = document.getElementById("reset-btn")
+    var resetBtn = document.getElementById("reset-btn");
     resetBtn.addEventListener('click', reset);
 });
+
+// https://stackoverflow.com/questions/77495555/how-do-i-execute-a-script-on-the-current-tab-using-chrome-scripting-api:
+// https://stackoverflow.com/questions/11684454/getting-the-source-html-of-the-current-page-from-chrome-extension/11696154#11696154
+// https://stackoverflow.com/questions/15195209/how-to-get-font-size-in-html
+// Prof. Lumbroso's code
